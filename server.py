71a3f5bd580aff289f699704c4210314cbd3b130
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-10-10 11:10:34 -0600

    changed index.html, added configs.yaml and requirements.txt with logging in server.py

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 import http.server
 import socketserver
+import yaml
+import logging
+import sys
 
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
+def runServer():
+    configsPath = '/Users/sharan_sreesai/Documents/webServer/'
+    serverConfigs = (row for row in open(configsPath+'configs.yaml'))
+    PORT = int(list(serverConfigs)[0].split("webServerPort :")[1])
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        logging.info("WEBSERVER RUNNING AT PORT : "+str(PORT))
+        httpd.serve_forever()
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+def main():
+    runServer()
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    main()
